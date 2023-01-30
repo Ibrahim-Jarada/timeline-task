9fe28f7a61fa1ab4d7aeb87c8406963d6cc5c408
ibrahim-jarada <ibrahim.jarada@gmail.com>
2023-01-30 15:48:14 +0200

    separate data into file and handling empty data case

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+// import data from './assets/data';
 import {Timeline} from './sections'
 const TestContainer = styled.div`
   display: flex;
@@ -8,49 +9,21 @@ const TestContainer = styled.div`
   padding: 32px;
   background-color: #faf9f7;
   margin: 0 auto;
+  
   @media only screen and (max-width: 600px) {
     width: 375px;
   }
 `;
+const data=[]
 function App() {
 
   return (
     <TestContainer>
-      <Timeline
-        title={"Assessment timeline"}
-        tasks={[
-          {
-            id: 1,
-            title: "Rater list to be submitted",
-            date: "15 December 2022",
-            status: "completed",
-          },
-          {
-            id: 2,
-            title: "Assessment window opens",
-            date: "18 December 2022",
-            status: "completed",
-          },
-          {
-            id: 3,
-            title: "Assessment window closes",
-            date: "20 December 2022",
-            status: "completed",
-          },
-          {
-            id: 4,
-            title: "Debrief due",
-            date: "22 December 2022",
-            status: "in-progress",
-          },
-          {
-            id: 5,
-            title: "Project closes",
-            date: "28 December 2022",
-            status: "todo",
-          },
-        ]}
-      />
+      {data?.length > 0 ? (
+        <Timeline title={"Assessment timeline"} tasks={data} />
+      ) : (
+        <h1>No Data Provided 😅</h1>
+      )}
     </TestContainer>
   );
 }
---
+++ src/assets/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Rater list to be submitted",
+        date: "15 December 2022",
+        status: "completed",
+    },
+    {
+        id: 2,
+        title: "Assessment window opens",
+        date: "18 December 2022",
+        status: "completed",
+    },
+    {
+        id: 3,
+        title: "Assessment window closes",
+        date: "20 December 2022",
+        status: "completed",
+    },
+    {
+        id: 4,
+        title: "Debrief due",
+        date: "22 December 2022",
+        status: "in-progress",
+    },
+    {
+        id: 5,
+        title: "Project closes",
+        date: "28 December 2022",
+        status: "todo",
+    },
+]
